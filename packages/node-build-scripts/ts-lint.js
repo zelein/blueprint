49f27f53eddbb333f93076b82b40ff0fcd55d03a
Gilad Gray <ggray@palantir.com>
2019-01-15 15:11:34 -0800

    ts-lint script uses eslint

--- packages/node-build-scripts/ts-lint.js
+++ packages/node-build-scripts/ts-lint.js
@@ -2,48 +2,23 @@
 
 /**
  * @license Copyright 2017 Palantir Technologies, Inc. All rights reserved.
- * @fileoverview Runs TSLint, with support for generating JUnit report
+ * @fileoverview Runs ESLint, with support for generating JUnit report
  */
 
 // @ts-check
 "use strict";
 
-const fs = require("fs");
-const { run } = require("tslint/lib/runner");
-const { Configuration } = require("tslint");
+/* eslint-disable typescript/no-var-requires */
+const { spawn } = require("child_process");
 const { junitReportPath } = require("./utils");
 
-let format = "codeFrame";
-let out;
-let outputStream = process.stdout;
+const args = [...process.argv.slice(2), "{src,test}/**/*.tsx"]
 if (process.env.JUNIT_REPORT_PATH != null) {
-    format = "junit";
-    out = junitReportPath("tslint");
-    console.info(`TSLint report will appear in ${out}`);
-    outputStream = fs.createWriteStream(out, { flags: "w+" });
+    const out = junitReportPath("tslint");
+    args.unshift("--output-file", out);
+    args.unshift("--format", "junit");
+    // eslint-disable-next-line no-console
+    console.info(`ESLint report will appear in ${out}`);
 }
 
-const configFile = Configuration.findConfiguration(null, __dirname).path;
-
-run({
-    config: configFile,
-    exclude: [],
-    files: ["{src,test}/**/*.tsx"],
-    fix: process.argv.includes("--fix"),
-    format,
-    out,
-}, {
-    error(m) {
-        process.stderr.write(m);
-    },
-    log(m) {
-        outputStream.write(m);
-    },
-})
-.then(status => {
-    process.exitCode = status;
-})
-.catch(e => {
-    console.error(e);
-    process.exitCode = 1;
-});
+spawn(`eslint`, args, { stdio: "inherit" })
